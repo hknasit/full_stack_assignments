5a56a4701520b099ac03e9e5b30edede859ef16f
Harsh Nasit <harshnasit1100@gmail.com>
2023-12-30 00:16:22 -0500

    Anagram
    The solution for finding the anagram words.

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,44 @@
 
 function isAnagram(str1, str2) {
 
+var isAnagramBoolean = true;
+
+const string1 = String(str1);
+const string2 = String(str2);
+
+var array1 = [];
+var array2 = [];
+
+for (let index = 0; index < string1.length; index++) {
+  const element = string1[index];
+  array1.push(element.toLocaleLowerCase());
+  
 }
+for (let index = 0; index < string2.length; index++) {
+  const element = string2[index];
+  array2.push(element.toLocaleLowerCase());
+}
+array1.sort();
+array2.sort();
+if(array1.length == array2.length){
+  for (let index = 0; index < array1.length; index++) {
+
+    if(array1[index] != array2[index]){
+      isAnagramBoolean = false;
+    }
+    
+  }
+}else{
+  isAnagramBoolean = false;
+}
+
+
+return isAnagramBoolean;
+
+}
+
+isAnagram("harsh Kishor", "Harsh kishor")
+
+
 
 module.exports = isAnagram;
